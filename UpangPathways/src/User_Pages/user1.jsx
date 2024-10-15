d627abb6837e68e486611e3647019d4a13c03634
unknown <almazanangel814@gmail.com>
2024-10-16 03:31:32 +0800

    all

--- UpangPathways/src/User_Pages/user1.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const user1 = () => {
-  return (
-    <div>user1</div>
-  )
-}
--- UpangPathways/src/User_Pages/user2.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const user2 = () => {
-  return (
-    <div>user2</div>
-  )
-}
